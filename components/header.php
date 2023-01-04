7ff84462691f44e31a08765dae5507688b4fea5b
cupidanish10 <cupidanish10@gmail.com>
2023-01-04 22:28:25 +0545

    menu dd added

--- components/header.php
+++ components/header.php
@@ -122,22 +122,25 @@
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                            <a class="nav-link" href="./index.php">Home</a>
 
                         </li>
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Product
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                        <li class="nav-item dropdown dropdown-mega position-static">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-auto-close="outside">Products</a>
+                            <div class="dropdown-menu brand-menu-shadow">
+                                <div class="mega-content">
+                                    <div class="container">
+                                        <div class="row">
+                                           
+
+
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li class="nav-item dropdown dropdown-mega position-static">
                             <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-auto-close="outside">Newsletter</a>
@@ -244,6 +247,7 @@
                                                             <div class="news-header">
                                                                 <img src="./img/blog3.jpeg" class="img-fluid" alt="">
                                                             </div>
+                                                            
 
                                                             <div class="news-body">
                                                                 <h3>
@@ -275,45 +279,38 @@
                         </li>
 
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Why us
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                        <li class="nav-item dropdown dropdown-mega position-static">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-auto-close="outside">Why Us</a>
+                            <div class="dropdown-menu brand-menu-shadow">
+                                <div class="mega-content">
+                                    <div class="container">
+                                        <div class="row">
+                                           
+
+
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
+                        <li class="nav-item dropdown dropdown-mega position-static">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-auto-close="outside">Help</a>
+                            <div class="dropdown-menu brand-menu-shadow">
+                                <div class="mega-content">
+                                    <div class="container">
+                                        <div class="row">
+                                           
 
 
 
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Help
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </li>
-
-
-
-
-
-
-
-
-
 
                     </ul>
 
