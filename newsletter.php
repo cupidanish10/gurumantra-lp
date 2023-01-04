c60dc79d47d60a65072ac58af14dd1557bbb7d34
cupidanish10 <cupidanish10@gmail.com>
2023-01-04 22:02:09 +0545

    newsletter list page added

---
+++ newsletter.php
@@ -0,0 +1,99 @@
+<!-- header starts -->
+<?php include 'components/header.php'; ?>
+<!-- header ends -->
+
+<main class="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title text-center">
+                    <h2> Our Newsletters </h2>
+
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-6">
+                <div class="card mb-3 news-list-item border-0 box-shadow-lg">
+                    <a href="" class="divLink"></a>
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <div class="img-cont">
+                                <img src="img/blog1.jpeg" class="img-fluid rounded-start" alt="...">
+
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title"> M1 High-Yield Savings Account with 4.50% APY on your cash</h5>
+                                <p class="card-text">Invest, Borrow, Spend—and now Save. Early next year, M1 will offer a high-yield savings account where M1 Plus members earn 4.50% APY1 on their cash. The M1 High-Yield Savings Account…</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mb-3 news-list-item border-0 box-shadow-lg">
+                    <a href="" class="divLink"></a>
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <div class="img-cont">
+                                <img src="img/blog2.jpeg" class="img-fluid rounded-start" alt="...">
+
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title"> A look back, a look ahead: Your year-end financial review guide</h5>
+                                <p class="card-text">As the year comes to a close, it’s important to review your finances and make necessary adjustments for the new year. Our guide covers everything you need to do.</p>
+                                <p class="card-text"><small class="text-muted">Jan 1, 2023</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-md-4">
+                <div class="card border-0 mb-3">
+                    <a href="" class="divLink"></a>
+                    <img src="img/blog1.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body ps-0">
+                        <h5 class="card-title">3 considerations for your financial plan in a high-interest rate market</h5>
+                        <p class="card-text"><small class="text-muted">November 21, 2022</small></p>
+                        <p class="card-text">As the Federal Reserve continues raising interest rates, we discuss three strategies you could include in your financial plan.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 mb-3">
+                    <a href="" class="divLink"></a>
+                    <img src="img/blog4.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body ps-0">
+                        <h5 class="card-title">3 ways to potentially lower your taxes before year-end</h5>
+                        <p class="card-text"><small class="text-muted">Dec 04, 2022</small></p>
+                        <p class="card-text">As 2022 comes to a close, we suggest some last minute ways to make the most out of your tax bill and set yourself up for next year.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 mb-3">
+                    <a href="" class="divLink"></a>
+                    <img src="img/blog3.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body ps-0">
+                        <h5 class="card-title">How automatic rebalancing simplifies portfolio management</h5>
+                        <p class="card-text"><small class="text-muted">Dec 11, 2022</small></p>
+                        <p class="card-text">Learn how we intelligently and automatically rebalances your portfolio to keep your financial targets and goals on track.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+
+<!-- footer starts -->
+<?php include 'components/footer.php'; ?>
+<!-- footer ends -->
